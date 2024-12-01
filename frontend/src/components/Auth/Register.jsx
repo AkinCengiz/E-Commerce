a3f3afc70cc8d8581fa0d9db2d09f1827666a20f
akincengiz <akincengiz@outlook.com.tr>
2024-12-01 10:58:40 +0300

    Register completed

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"
+
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [formData,setFormData] = useState({
+    username : "",
+    email : "",
+    password : ""
+  });
+
+  const handleInputChange = (e) => {
+    const {name,value} = e.target;
+    setFormData({...formData,[name]:value})
+  }
+
+  const handleRegister = async(e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/users/register",{
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(formData)
+      });
+
+      if(response.ok){
+        const result = await response.json();
+        localStorage.setItem("user",JSON.stringify(result));
+        navigate("/");
+      }else{
+        console.log("Kayıt işleminde hata meydana geldi...")
+      }
+
+    } catch (error) {
+      console.log("Sunucu hatası...",error)
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleRegister}>
       <div>
         <label>
           <span>
             Username <span className="required">*</span>
           </span>
-          <input type="text" />
+          <input type="text" name="username" onChange={handleInputChange} />
         </label>
       </div>
       <div>
@@ -16,7 +54,7 @@ const Register = () => {
           <span>
             Email address <span className="required">*</span>
           </span>
-          <input type="email" />
+          <input type="email" name="email" onChange={handleInputChange}/>
         </label>
       </div>
       <div>
@@ -24,7 +62,7 @@ const Register = () => {
           <span>
             Password <span className="required">*</span>
           </span>
-          <input type="password" />
+          <input type="password" name="password" onChange={handleInputChange}/>
         </label>
       </div>
       <div className="privacy-policy-text remember">
